cfc664ad2fc400be9b8a52251bba603b03bd1711
Lindsey Rappaport <84059264+lindseyrapp@users.noreply.github.com>
2025-03-05 20:00:22 -0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
-const blogCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        pubDate: z.date(),
-        description: z.string().optional(),
-        tags: z.array(z.string()).optional(),
-    }),
+const blog = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
 });
 
-export const collections = {
-    blog: blogCollection,
-};
+export const collections = { blog };
